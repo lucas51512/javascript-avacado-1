f97e393671d9c37393f5285ab78df48a7d24e188
apotecario1277 <lucasbrancalhao454@gmail.com>
2020-02-27 17:03:42 -0300

    class NegociacaoController

---
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -0,0 +1,23 @@
+class NegociacaoController {
+
+  constructor(){
+
+    let $ = document.querySelector.bind(document);
+    this._inputData = $('#data');
+    this._inputQuantidade = $('#quantidade');
+    this._inputValor = $('#valor');
+
+
+  }
+
+  adiciona(event){
+    event.preventDefault();
+
+
+    console.log(this._inputData.value);
+    console.log(this._inputQuantidade.value);
+    console.log(this._inputValor.value);
+
+  }
+
+}
--- aluraframe/client/js/app/models/Negociacao.js
+++ aluraframe/client/js/app/models/Negociacao.js
@@ -1,8 +1,20 @@
 class Negociacao {
-  constructor() {
-      this.date = new Date();
-      this.quantidade = 1;
-      this.valor = 0; 
+  constructor(data, quantidade, valor) {
+      this._data = new Date(data.getTime());
+      this._quantidade = quantidade;
+      this._valor = valor;
+      Object.freeze(this);
 
   }
+    get data(){
+      return new Date(this._data.getTime());
+    }
+
+    get quantidade(){
+      return this._quantidade;
+    }
+
+    get valor(){
+      return this._valor;
+    }
 }
