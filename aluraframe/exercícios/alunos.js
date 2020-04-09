4ba15ceb30e4bae9a1436e388ef9c30b439ae4e9
lucas51512 <lucas51512@users.noreply.github.com>
2020-04-09 13:20:42 -0300

    exercicios

---
+++ aluraframe/exercícios/alunos.js
@@ -0,0 +1,10 @@
+let avaliacoes = [
+  new Prova(new Aluno(1, 'Luana'), 8),
+  new Prova(new Aluno(2, 'Cássio'), 6),
+  new Prova(new Aluno(3, 'Barney'), 9),
+  new Prova(new Aluno(4, 'Bira'), 5),
+]
+
+let aprovados = avaliacoes.filter((prova) => prova.nota >= 7).map((prova) => prova.aluno.nome);
+
+console.log(aprovados);
---
+++ aluraframe/exercícios/classe_Aluno.js
@@ -0,0 +1,13 @@
+class Aluno {
+  constructor(matricula, nome) {
+    this.matricula = matricula;
+    this.nome = nome;
+  }
+}
+
+class Prova {
+  constructor(aluno, nota){
+    this.aluno = aluno;
+    this.nota = nota;
+  }
+}
